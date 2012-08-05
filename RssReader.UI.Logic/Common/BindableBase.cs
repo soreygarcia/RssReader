e0acee0e76011a8cd3991cb0d324d8e26f0aca4c
SoreyGarcia <soreygarcia@gmail.com>
2012-08-05 15:14:16 -0500

    Ensamblando la infraestructura
    Codigo de la aplicación en el tercer post
    http://blog.soreygarcia.me/2012/08/ensamblando-la-infraestructura-de.html

---
+++ RssReader.UI.Logic/Common/BindableBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+
+namespace RssReader.UI.Logic.Common
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private bool _isWorking;
+        public bool IsWorking
+        {
+            set
+            {
+                this._isWorking = value;
+                NotifyPropertyChanged("IsWorking");
+            }
+            get
+            {
+                return _isWorking;
+            }
+        }
+
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            set
+            {
+                this._statusMessage = value;
+                NotifyPropertyChanged("StatusMessage");
+            }
+            get
+            {
+                return _statusMessage;
+            }
+        }
+    }
+}
